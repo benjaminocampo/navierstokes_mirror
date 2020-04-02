14df39675fe69ce0837dd63435d99bb9e8935d8d
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2020-04-02 12:08:54 -0300

    Switch between branches using python.

--- utils.py
+++ utils.py
@@ -1,14 +1,28 @@
 import pdb
 import sys
+import subprocess
 import pandas as pd
-import matplotlib as plt
+import git
+from io import StringIO 
+from pathlib import Path
 
 def main():
-    data = pd.read_csv(sys.stdin)
-    total_ns = data["total_ns"]
-    react = data["react"]
-    vel_step = data["vel_step"]
-    dens_step = data["dens_step"]
+    repo_abs_path = str(Path(".").resolve())
+    repo = git.Repo(repo_abs_path)
+    subprocess.run(['make', 'clean'])
+    subprocess.run(['make'])
+    result = subprocess.run('./headless', stdout=subprocess.PIPE).stdout.decode('utf-8')
+    data = StringIO(result)
+    data = pd.read_csv(data)
+    print(data)
+    repo.git.checkout('test_branch')
+    subprocess.run(['make', 'clean'])
+    subprocess.run(['make'])
+    result2 = subprocess.run('./headless', stdout=subprocess.PIPE).stdout.decode('utf-8')
+    data2 = StringIO(result2)
+    data2 = pd.read_csv(data2)
+    print(data2)
+
 
 
 if __name__ == "__main__":
