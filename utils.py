d6f8bf3981c5706ed8bdde9fc0b0d94712c707fc
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2020-04-02 21:20:57 -0300

    Check if the repository has uncommited changes before checking out.

--- utils.py
+++ utils.py
@@ -6,24 +6,34 @@ import git
 from io import StringIO 
 from pathlib import Path
 
-def main():
-    repo_abs_path = str(Path(".").resolve())
-    repo = git.Repo(repo_abs_path)
+def get_data_from_stdin():
     subprocess.run(['make', 'clean'])
     subprocess.run(['make'])
     result = subprocess.run('./headless', stdout=subprocess.PIPE).stdout.decode('utf-8')
     data = StringIO(result)
     data = pd.read_csv(data)
-    print(data)
+    return data
+
+def main():
+    # Get absolute path of the repository.
+    repo_abs_path = str(Path(".").resolve())
+
+    # Track the repository by means of the absolute path.
+    repo = git.Repo(repo_abs_path)
+    
+    if repo.is_dirty():
+        print("There are uncommited changes. You cannot switch between branches.", 
+            file=sys.stderr)
+        sys.exit(1)
+
+    # Get measurementes given by the current program on branch master
+    current_info = get_data_from_stdin()
+
+    # Checkout to another branch
     repo.git.checkout('test_branch')
-    subprocess.run(['make', 'clean'])
-    subprocess.run(['make'])
-    result2 = subprocess.run('./headless', stdout=subprocess.PIPE).stdout.decode('utf-8')
-    data2 = StringIO(result2)
-    data2 = pd.read_csv(data2)
-    print(data2)
-
-
+    
+    # Get measurements given by the new branch
+    likely_improvement_info = get_data_from_stdin()
 
 if __name__ == "__main__":
     main()
