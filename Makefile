3f957f4a4600ca990470a250ad48d95bd52c945f
Mateo de Mayo <mateodemayo@gmail.com>
2020-04-04 16:33:18 -0300

    Set baseline branch

--- Makefile
+++ Makefile
@@ -1,25 +1,50 @@
+# TODO: Try -ffast-math
 CC=cc
-CFLAGS=-std=c99 -Wall -Wextra -Wno-unused-parameter
+override CFLAGS:=-std=c99 -Wall -Wextra -Werror -Wno-unused-parameter $(CFLAGS)
 LDFLAGS=
 
 TARGETS=demo headless
 SOURCES=$(shell echo *.c)
 COMMON_OBJECTS=timing.o solver.o
 
+.PHONY: clean
 all: $(TARGETS)
 
 demo: demo.o $(COMMON_OBJECTS)
 	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -lGL -lGLU -lglut
 
+# make headless CFLAGS='-Ofast -march=native -floop-nest-optimize -funroll-loops -flto -g'
 headless: headless.o $(COMMON_OBJECTS)
 	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
 
-clean:
-	rm -f $(TARGETS) *.o .depend *~
+asm: solver.o
+	$(CC) $(CFLAGS) -fno-asynchronous-unwind-tables -fno-exceptions -S solver.c
+
+runperf: headless
+	sudo perf stat \
+	-e \
+	cache-references,\
+	cache-misses,\
+	L1-dcache-stores,\
+	L1-dcache-store-misses,\
+	LLC-stores,\
+	LLC-store-misses,\
+	page-faults,\
+	cycles,\
+	instructions,\
+	branches,\
+	branch-misses \
+	-ddd \
+	./headless
 
 .depend: *.[ch]
-	$(CC) -MM $(SOURCES) >.depend
+	$(CC) -MM $(SOURCES) > .depend
 
 -include .depend
 
-.PHONY: clean all
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@ $(LDFLAGS)
+
+.PHONY: clean
+clean:
+	rm -f $(TARGETS) *.o .depend solver.s *~
--- headless.c
+++ headless.c
@@ -16,27 +16,36 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "timing.h"
 
 /* macros */
 
-#define IX(i,j) ((i)+(N+2)*(j))
+#define IX(i,j) ((j)+(N+2)*(i))
 
 /* external definitions (from solver.c) */
 
+// TODO: Why extern instead of include solver.h
 extern void dens_step ( int N, float * x, float * x0, float * u, float * v, float diff, float dt );
 extern void vel_step ( int N, float * u, float * v, float * u0, float * v0, float visc, float dt );
 
 /* global variables */
 
-static int N;
+static int N, steps;
 static float dt, diff, visc;
 static float force, source;
 
 static float * u, * v, * u_prev, * v_prev;
 static float * dens, * dens_prev;
 
+__attribute__((unused))
+static void dump_grid(float * grid) {
+	for (int i = 0; i < N; i++) {
+		printf("%d [", i);
+		for (int j = 0; j < N; j++)
+			printf("%f, ", grid[IX(i, j)]);
+		printf("], \n");
+	}
+}
 
 /*
   ----------------------------------------------------------------------
@@ -101,54 +110,45 @@ static void react ( float * d, float * u, float * v )
 		}
 	}
 
+	// TODO: Unify these two fors
 	for ( i=0 ; i<size ; i++ ) {
 		u[i] = v[i] = d[i] = 0.0f;
 	}
 
 	if (max_velocity2<0.0000005f) {
-		u[IX(N/2,N/2)] = force * 10.0f;
-		v[IX(N/2,N/2)] = force * 10.0f;
+		u[IX(N/2,N/2)] = force * 1000.0f;
+		v[IX(N/2,N/2)] = force * 1000.0f;
 	}
 	if (max_density<1.0f) {
-		d[IX(N/2,N/2)] = source * 10.0f;
+		d[IX(N/2,N/2)] = source * 1000.0f;
 	}
-
 	return;
 }
 
 static void one_step ( void )
 {
-	static int times = 1;
+	//static int times = 1;
 	static double start_t = 0.0;
-	static double one_second = 0.0;
+	//static double one_second = 0.0;
 	static double react_ns_p_cell = 0.0;
 	static double vel_ns_p_cell = 0.0;
 	static double dens_ns_p_cell = 0.0;
-
+	 
 	start_t = wtime();
 	react ( dens_prev, u_prev, v_prev );
-	react_ns_p_cell += 1.0e9 * (wtime()-start_t)/(N*N);
+	react_ns_p_cell = 1.0e9 * (wtime()-start_t)/(N*N);
 
 	start_t = wtime();
 	vel_step ( N, u, v, u_prev, v_prev, visc, dt );
-	vel_ns_p_cell += 1.0e9 * (wtime()-start_t)/(N*N);
+	vel_ns_p_cell = 1.0e9 * (wtime()-start_t)/(N*N);
 
 	start_t = wtime();
 	dens_step ( N, dens, dens_prev, u, v, diff, dt );
-	dens_ns_p_cell += 1.0e9 * (wtime()-start_t)/(N*N);
+	dens_ns_p_cell = 1.0e9 * (wtime()-start_t)/(N*N);
 
-	if (1.0<wtime()-one_second) { /* at least 1s between stats */
-		printf("%lf, %lf, %lf, %lf: ns per cell total, react, vel_step, dens_step\n",
-			(react_ns_p_cell+vel_ns_p_cell+dens_ns_p_cell)/times,
-			react_ns_p_cell/times, vel_ns_p_cell/times, dens_ns_p_cell/times);
-		one_second = wtime();
-		react_ns_p_cell = 0.0;
-		vel_ns_p_cell = 0.0;
-		dens_ns_p_cell = 0.0;
-		times = 1;
-	} else {
-		times++;
-	}
+	printf("%lf, %lf, %lf, %lf\n",
+			(react_ns_p_cell+vel_ns_p_cell+dens_ns_p_cell),
+			react_ns_p_cell, vel_ns_p_cell, dens_ns_p_cell);
 }
 
 
@@ -162,7 +162,7 @@ int main ( int argc, char ** argv )
 {
 	int i = 0;
 
-	if ( argc != 1 && argc != 7 ) {
+	if ( argc != 1 && argc != 8 ) {
 		fprintf ( stderr, "usage : %s N dt diff visc force source\n", argv[0] );
 		fprintf ( stderr, "where:\n" );\
 		fprintf ( stderr, "\t N      : grid resolution\n" );
@@ -171,18 +171,21 @@ int main ( int argc, char ** argv )
 		fprintf ( stderr, "\t visc   : viscosity of the fluid\n" );
 		fprintf ( stderr, "\t force  : scales the mouse movement that generate a force\n" );
 		fprintf ( stderr, "\t source : amount of density that will be deposited\n" );
+		fprintf ( stderr, "\t steps : amount of steps the program will be executed\n" );
 		exit ( 1 );
 	}
 
 	if ( argc == 1 ) {
 		N = 128;
 		dt = 0.1f;
-		diff = 0.0f;
-		visc = 0.0f;
+		diff = 0.001f;
+		visc = 0.0001f;
 		force = 5.0f;
 		source = 100.0f;
-		fprintf ( stderr, "Using defaults : N=%d dt=%g diff=%g visc=%g force = %g source=%g\n",
-			N, dt, diff, visc, force, source );
+		steps = 5;
+		
+		fprintf ( stderr, "Using defaults : N=%d dt=%g diff=%g visc=%g force = %g source=%g steps=%d\n",
+			N, dt, diff, visc, force, source, steps);
 	} else {
 		N = atoi(argv[1]);
 		dt = atof(argv[2]);
@@ -190,13 +193,14 @@ int main ( int argc, char ** argv )
 		visc = atof(argv[4]);
 		force = atof(argv[5]);
 		source = atof(argv[6]);
+		steps = atoi(argv[7]);
 	}
 
 	if ( !allocate_data () ) exit ( 1 );
 	clear_data ();
-	for (i=0; i<2048; i++)
+	printf("total_ns,react,vel_step,dens_step\n");
+	for (i=0; i<steps; i++)
 		one_step ();
 	free_data ();
-
 	exit ( 0 );
 }
