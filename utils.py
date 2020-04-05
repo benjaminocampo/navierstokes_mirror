3d034f934c8c9dba3612657209b6f2e60ff0adb0
Mateo de Mayo <mateodemayo@gmail.com>
2020-04-05 16:30:24 -0300

    Improve default parameters and outputs. Show executable output in realtime

--- utils.py
+++ utils.py
@@ -5,74 +5,8 @@ import git
 import shlex
 import pandas as pd
 import statsmodels.api as sm
-from io import StringIO
+from io import StringIO, TextIOWrapper
 from pathlib import Path
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument(
-    '-sb',
-    '--sbranch',
-    nargs='?',
-    help='name of the source branch')
-
-parser.add_argument(
-    '-smake',
-    required=True,
-    help='make-commands to be run by the source branch'
-)
-
-parser.add_argument(
-    '-tb',
-    '--tbranch',
-    help='name of the target branch')
-
-parser.add_argument(
-    '-tmake',
-    required=True,
-    help='make-commands to be run by the target branch'
-)
-
-parser.add_argument(
-    '-sexec',
-    nargs='?',
-    default=None,
-    help='executable command to be run by the source branch')
-
-parser.add_argument(
-    '-texec',
-    nargs='?',
-    default=None,
-    help='executable command to be run by the target branch')
-
-parser.add_argument(
-    '-n',
-    nargs="?",
-    default="128",
-    help="length of the square grid"
-)
-
-parser.add_argument(
-    '-steps',
-    nargs="?",
-    default="4",
-    help="amount of steps to simulate"
-)
-
-parser.add_argument(
-    '-csv',
-    action='store_true',
-    help='write measurements to a csv file.')
-
-parser.add_argument(
-    '-p',
-    '--plot',
-    action='store_true',
-    help='plot graphics.')
-
-
-import statsmodels.api as sm
-
 
 def test_hypotheses(sample_hyp1, sample_hyp2):
     """
@@ -122,74 +56,141 @@ def get_data_from_stdin(make_cmds, exec_cmds):
     data: Panda DataFrame object
         measurements obtained by stdout after running exec_cmds.
     """
-    try:
-        subprocess.run(['make', 'clean'])
-        subprocess.run(shlex.split(make_cmds))
-        result = subprocess.run(
-            shlex.split(exec_cmds),
-            stdout=subprocess.PIPE,
-            shell=True).stdout.decode('utf-8')
-    except subprocess.CalledProcessError as e:
-        print('Fail execution of', e, file=sys.stderr)
-        sys.exit(1)
+    subprocess.run(['make', 'clean'])
+    print(f">>> [MAKE] {make_cmds}")
+    subprocess.run(shlex.split(make_cmds))
+    print(f">>> [EXEC] {exec_cmds}")
+    run = subprocess.Popen(shlex.split(exec_cmds), stdout=subprocess.PIPE)
 
-    data = StringIO(result)
-    data = pd.read_csv(data)
+    # Read while printing output
+    output = StringIO()
+    for line in TextIOWrapper(run.stdout, encoding="utf-8", line_buffering=True, write_through=True):
+        output.write(line)
+        print(line, end="", flush=True)
+    output.seek(0)
+
+    data = pd.read_csv(output)
     return data
 
+def  get_parser(active_branch, default_n=2048, default_steps=4):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-sb',
+        '--sbranch',
+        nargs='?',
+        default=active_branch,
+        help='name of the source branch, current one by default'
+    )
+    parser.add_argument(
+        '-smake',
+        required=True,
+        help='make-commands to be run by the source branch'
+    )
+    parser.add_argument(
+        '-tb',
+        '--tbranch',
+        nargs='?',
+        default=active_branch,
+        help='name of the target branch, current one by default'
+    )
+    parser.add_argument(
+        '-tmake',
+        required=True,
+        help='make-commands to be run by the target branch'
+    )
+    parser.add_argument(
+        '-sexec',
+        nargs='?',
+        default=None,
+        help='executable command to be run by the source branch'
+    )
+    parser.add_argument(
+        '-texec',
+        nargs='?',
+        default=None,
+        help='executable command to be run by the target branch'
+    )
+    parser.add_argument(
+        '-n',
+        nargs="?",
+        default=default_n,
+        help="length of the square grid"
+    )
+    parser.add_argument(
+        '-steps',
+        nargs="?",
+        default=default_steps,
+        help="amount of steps to simulate"
+    )
+    parser.add_argument(
+        '-csv',
+        action='store_true',
+        help='[TO BE DONE] write measurements to a csv file.'
+    )
+    parser.add_argument(
+        '-p',
+        '--plot',
+        action='store_true',
+        help='[TO BE DONE] plot graphics.'
+    )
+    return parser
 
 def main():
     repo_abs_path = str(Path(".").resolve())
     repo = git.Repo(repo_abs_path)
-
-    args = parser.parse_args()
     initial_branch = repo.active_branch.name
-    sbranch = initial_branch if args.sbranch is None else args.sbranch
+    args = get_parser(initial_branch).parse_args()
+
+    sbranch = args.sbranch
     tbranch = args.tbranch
-    smake_cmds = args.smake
     n = args.n
     steps = args.steps
-    sexec_cmds = args.sexec
-    if sexec_cmds is None and n is not None and steps is not None:
-        sexec_cmds = f"./headless {n} 0.1 0.001 0.0001 5.0 100.0 {steps}"
-    tmake_cmds = args.tmake
-    texec_cmds = args.texec
-    if texec_cmds is None and n is not None and steps is not None:
-        texec_cmds = f"./headless {n} 0.1 0.001 0.0001 5.0 100.0 {steps}"
+    default_exec = f"./headless {n} 0.1 0.001 0.0001 5.0 100.0 {steps}"
+    sexec = args.sexec or default_exec
+    texec = args.texec or default_exec
+    smake = args.smake
+    tmake = args.tmake
+    print(
+        f"Running with the following configuration:\n"
+        f"Source branch={sbranch}\n"
+        f"Source make={smake}\n"
+        f"Source exec={sexec}\n"
+        f"Target branch={tbranch}\n"
+        f"Target make={tmake}\n"
+        f"Target exec={texec}\n"
+    )
 
-    if tbranch is None:
-        source_info = get_data_from_stdin(smake_cmds, sexec_cmds)
-        target_info = get_data_from_stdin(tmake_cmds, texec_cmds)
-    else:
-        failed = False
-        was_dirty = repo.is_dirty()
+    failed = False
+    was_dirty = repo.is_dirty()
+    if was_dirty:
+        repo.git.stash()
+    try:
+        repo.git.checkout(sbranch)
+        source_info = get_data_from_stdin(smake, sexec)
+        repo.git.checkout(tbranch)
+        target_info = get_data_from_stdin(tmake, texec)
+    except BaseException as e:
+        failed = True
+        print(f"There was an error in the execution: {repr(e)}")
+    finally:
+        repo.git.checkout(initial_branch)
         if was_dirty:
-            repo.git.stash()
-        try:
-            repo.git.checkout(sbranch)
-            source_info = get_data_from_stdin(smake_cmds, sexec_cmds)
-            repo.git.checkout(tbranch)
-            target_info = get_data_from_stdin(tmake_cmds, texec_cmds)
-        except BaseException as e:
-            failed = True
-            print(f"There was an error in the execution: {repr(e)}")
-        finally:
-            repo.git.checkout(initial_branch)
-            if was_dirty:
-                repo.git.stash("pop")
-        if failed:
-            exit(1)
+            repo.git.stash("pop")
+    if failed:
+        exit(1)
 
     significance_level = 0.01
     mu0, observed_value, p_value, ratio = test_hypotheses(source_info['total_ns'],
                                                           target_info['total_ns'])
     if p_value < significance_level:
-        print('Assert in favour of the new approach.')
+        print(f"P value: {p_value} < {significance_level}")
+        print("Assert in favour of the new approach.")
     else:
-        print('No evidence to reject the previous approach.')
-    print("Mu0:", mu0)
-    print("Observed_value:", observed_value)
-    print(f"Ratio: {ratio}%")
+        print(f"P value: {p_value} >= {significance_level}")
+        print("No evidence to reject the previous approach.")
+    print("Source mean:", mu0)
+    print("Target mean:", observed_value)
+    print(f"Percentage improved from source to target: {ratio:.2f}%")
 
 
 if __name__ == "__main__":
