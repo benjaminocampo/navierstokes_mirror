2edbea9bb51e516e37d4d0a1ffe4a58602544502
Mateo de Mayo <mateodemayo@gmail.com>
2020-05-12 18:45:12 -0300

    Rewrite everything for non avx cpus

--- intrinsics_helpers.h
+++ intrinsics_helpers.h
@@ -4,132 +4,132 @@
 
 // Floating point helpers
 
-static inline void dump_pfloats(__m256 ps) {
+static inline void dump_pfloats(__m128 ps) {
   float as[8];
-  _mm256_storeu_ps(&as[0], ps);
+  _mm_storeu_ps(&as[0], ps);
   printf("[%f, %f, %f, %f, %f, %f, %f, %f]\n", as[0], as[1], as[2], as[3],
          as[4], as[5], as[6], as[7]);
 }
 
-static inline int fiszero(__m256 ps) {
+static inline int fiszero(__m128 ps) {
   float as[8];
-  _mm256_storeu_ps(&as[0], ps);
+  _mm_storeu_ps(&as[0], ps);
   return as[0] == 0 && as[1] == 0 && as[2] == 0 && as[3] == 0 && as[4] == 0 &&
          as[5] == 0 && as[6] == 0 && as[7] == 0;
 }
 
 // Unaligned 256b load
-static inline __m256 fload(float const *base_addr) {
-  return _mm256_loadu_ps(base_addr);
+static inline __m128 fload(float const *base_addr) {
+  return _mm_loadu_ps(base_addr);
 }
 
 // Unaligned 256b store
-static inline void fstore(float *base_addr, __m256 data) {
-  _mm256_storeu_ps(base_addr, data);
+static inline void fstore(float *base_addr, __m128 data) {
+  _mm_storeu_ps(base_addr, data);
 }
 
 // Aligned 256b load
-static inline __m256 faload(float const *base_addr) {
-  return _mm256_load_ps(base_addr);
+static inline __m128 faload(float const *base_addr) {
+  return _mm_load_ps(base_addr);
 }
 
 // Aligned 256b store
-static inline void fastore(float *base_addr, __m256 data) {
-  _mm256_store_ps(base_addr, data);
+static inline void fastore(float *base_addr, __m128 data) {
+  _mm_store_ps(base_addr, data);
 }
 
-// equivalent to _mm256_loadu2_m128
-static inline __m256 fload2x4(float const *base_addr) {
-  __m256 __v256 = _mm256_castps128_ps256(_mm_loadu_ps(base_addr));
-  return _mm256_insertf128_ps(__v256, _mm_loadu_ps(base_addr + 4), 1);
+// equivalent to _mm_loadu2_m128
+static inline __m128 fload2x4(float const *base_addr) {
+  return fload(base_addr);
 }
 
-static inline __m256 fmul(__m256 a, __m256 b) { return _mm256_mul_ps(a, b); }
-static inline __m256 fadd(__m256 a, __m256 b) { return _mm256_add_ps(a, b); }
-static inline __m256 fsub(__m256 a, __m256 b) { return _mm256_sub_ps(a, b); }
+static inline __m128 fmul(__m128 a, __m128 b) { return _mm_mul_ps(a, b); }
+static inline __m128 fadd(__m128 a, __m128 b) { return _mm_add_ps(a, b); }
+static inline __m128 fsub(__m128 a, __m128 b) { return _mm_sub_ps(a, b); }
 // The p prefix is for conflicts with math.h
-static inline __m256 pfmax(__m256 a, __m256 b) { return _mm256_max_ps(a, b); }
+static inline __m128 pfmax(__m128 a, __m128 b) { return _mm_max_ps(a, b); }
 // The p prefix is for conflicts with math.h
-static inline __m256 pfmin(__m256 a, __m256 b) { return _mm256_min_ps(a, b); }
+static inline __m128 pfmin(__m128 a, __m128 b) { return _mm_min_ps(a, b); }
 
-static inline __m256 fclamp(__m256 t, __m256 a, __m256 b) {
+static inline __m128 fclamp(__m128 t, __m128 a, __m128 b) {
   return pfmin(pfmax(a, t), b);
 }
 
+static inline __m128 fset(float x3, float x2, float x1, float x0) {
+  return _mm_set_ps(x3, x2, x1, x0);
+}
+
+static inline __m128 fset1(float a) { return _mm_set1_ps(a); }
+
 // a * x + y
-static inline __m256 ffmadd(__m256 a, __m256 x, __m256 y) {
-  return _mm256_fmadd_ps(a, x, y);
+static inline __m128 ffmadd(__m128 a, __m128 x, __m128 y) {
+  // return _mm_fmadd_ps(a, x, y);
+  return fadd(fmul(a, x), y);
 }
 
 // -(a * x) + y
-static inline __m256 ffnmadd(__m256 a, __m256 x, __m256 y) {
-  return _mm256_fnmadd_ps(a, x, y);
+static inline __m128 ffnmadd(__m128 a, __m128 x, __m128 y) {
+  // return _mm_fnmadd_ps(a, x, y);
+  return fadd(fmul(fset1(-1.0f), fmul(a, x)), y);
 }
-
-static inline __m256 fset(float x7, float x6, float x5, float x4, float x3,
-                          float x2, float x1, float x0) {
-  return _mm256_set_ps(x7, x6, x5, x4, x3, x2, x1, x0);
-}
-
-static inline __m256 fset1(float a) { return _mm256_set1_ps(a); }
 
 // shift right: if a = [ 8 7 6 5 | 4 3 2 1 ] -> returns [ 0 8 7 6 | 5 4 3 2 ]
-static inline __m256 fshr(__m256 a) {
-  __m256 t0 = _mm256_permute_ps(a, 0b00111001);  // [ 5 8 7 6 | 1 4 3 2 ]
-  __m256 t1 = _mm256_permute2f128_ps(t0, t0, 0b10000001);  // [0 0 0 0 5 8 7 6]
-  __m256 sr = _mm256_blend_ps(t0, t1, 0b10001000);  // [ 0 8 7 6 | 5 4 3 2 ]
-  return sr;
+static inline __m128 fshr(__m128 a) {
+  __m128 t0 = _mm_blend_ps(a, fset1(0), 0b0001);  // [ 4 3 2 0 ]
+  __m128 t1 = _mm_shuffle_ps(t0, t0, 0b00111001);     // [ 0 4 3 2 ]
+  return t1;
 }
 
 // shift left: if a = [ 8 7 6 5 | 4 3 2 1 ] -> returns [ 7 6 5 4 | 3 2 1 0 ]
-static inline __m256 fshl(__m256 a) {
-  __m256 t0 = _mm256_permute_ps(a, 0b10010011);  // [ 7 6 5 8 | 3 2 1 4 ]
-  __m256 t1 = _mm256_permute2f128_ps(t0, t0, 0b00001000);  // [3 2 1 4 0 0 0 0]
-  __m256 sl = _mm256_blend_ps(t0, t1, 0b00010001);  // [ 7 6 5 4 | 3 2 1 0 ]
-  return sl;
+static inline __m128 fshl(__m128 a) {
+  __m128 t0 = _mm_blend_ps(a, fset1(0), 0b1000);  // [ 0 3 2 1 ]
+  __m128 t1 = _mm_shuffle_ps(t0, t0, 0b10010011);     // [ 3 2 1 0 ]
+  return t1;
 }
 
 // Integer helpers
 
-static inline void dump_pints(__m256i epi) {
+static inline void dump_pints(__m128i epi) {
   int as[8];
-  _mm256_storeu_si256((__m256i *)&as[0], epi);
+  _mm_storeu_si128((__m128i *)&as[0], epi);
   printf("[%d, %d, %d, %d, %d, %d, %d, %d]\n", as[0], as[1], as[2], as[3],
          as[4], as[5], as[6], as[7]);
 }
 
-static inline __m256i iadd(__m256i a, __m256i b) {
-  return _mm256_add_epi32(a, b);
+static inline __m128i iadd(__m128i a, __m128i b) { return _mm_add_epi32(a, b); }
+
+static inline __m128i isub(__m128i a, __m128i b) { return _mm_sub_epi32(a, b); }
+
+static inline __m128i imul(__m128i a, __m128i b) {
+  return _mm_mullo_epi32(a, b);
 }
 
-static inline __m256i isub(__m256i a, __m256i b) {
-  return _mm256_sub_epi32(a, b);
+static inline __m128i ixor(__m128i a, __m128i b) { return _mm_xor_si128(a, b); }
+
+static inline __m128i iand(__m128i a, __m128i b) { return _mm_and_si128(a, b); }
+
+static inline __m128i ior(__m128i a, __m128i b) { return _mm_or_si128(a, b); }
+
+static inline __m128i iset(int x3, int x2, int x1, int x0) {
+  return _mm_set_epi32(x3, x2, x1, x0);
 }
 
-static inline __m256i imul(__m256i a, __m256i b) {
-  return _mm256_mullo_epi32(a, b);
-}
-
-static inline __m256i ixor(__m256i a, __m256i b) {
-  return _mm256_xor_si256(a, b);
-}
-
-static inline __m256i iand(__m256i a, __m256i b) {
-  return _mm256_and_si256(a, b);
-}
-
-static inline __m256i ior(__m256i a, __m256i b) {
-  return _mm256_or_si256(a, b);
-}
-
-static inline __m256i iset(int x7, int x6, int x5, int x4, int x3, int x2,
-                           int x1, int x0) {
-  return _mm256_set_epi32(x7, x6, x5, x4, x3, x2, x1, x0);
-}
-
-static inline __m256i iset1(int x) { return _mm256_set1_epi32(x); }
+static inline __m128i iset1(int x) { return _mm_set1_epi32(x); }
 
 // Miscelaneous
 
-static inline __m256 itof(__m256i x) { return _mm256_cvtepi32_ps(x); }
-static inline __m256i ftoi(__m256 x) { return _mm256_cvttps_epi32(x); }
+static inline __m128 itof(__m128i x) { return _mm_cvtepi32_ps(x); }
+static inline __m128i ftoi(__m128 x) { return _mm_cvttps_epi32(x); }
+
+static inline __m128 fgather(const float *base, __m128i indexes, float scale) {
+  int a = _mm_extract_epi32(indexes, 0);
+  int b = _mm_extract_epi32(indexes, 1);
+  int c = _mm_extract_epi32(indexes, 2);
+  int d = _mm_extract_epi32(indexes, 3);
+  return fset(base[d], base[c], base[b], base[a]);
+}
+
+// i := j*32
+// m := j*32
+// addr := base_addr + SignExtend64(vindex[m+31:m]) * ZeroExtend64(scale) * 8
+// dst[i+31:i] := MEM[addr+31:addr]
--- solver.c
+++ solver.c
@@ -19,13 +19,13 @@ typedef enum { RED, BLACK } grid_color;
 
 static void add_source(unsigned int n, float *x, const float *s, float dt) {
   unsigned int size = (n + 2) * (n + 2);
-  const __m256 pdt = fset1(dt);
+  const __m128 pdt = fset1(dt);
   unsigned int i;
-  for (i = 0; i < size - 8; i += 8) {
-    __m256 px = faload(&x[i]);
-    __m256 ps = faload(&s[i]);
-    __m256 product = _mm256_fmadd_ps(pdt, ps, px);  // x + dt * s[i]
-    _mm256_store_ps(&x[i], product);                // x[i] += dt * s[i];
+  for (i = 0; i < size - 4; i += 4) {
+    __m128 px = faload(&x[i]);
+    __m128 ps = faload(&s[i]);
+    __m128 product = ffmadd(pdt, ps, px);  // x + dt * s[i]
+    fastore(&x[i], product);                // x[i] += dt * s[i];
   }
   for (; i < size; i++) x[i] += dt * s[i];
 }
@@ -50,27 +50,27 @@ static void lin_solve_rb_step(grid_color color, unsigned int n, float a,
   const float invc = 1 / c;
   unsigned int start = color == RED ? 0 : 1;
   unsigned int width = (n + 2) / 2;
-  const __m256 pinvc = fset1(invc);
-  const __m256 pa = fset1(a);
+  const __m128 pinvc = fset1(invc);
+  const __m128 pa = fset1(a);
   float leftmost;  // Left slack of the left-right neighbours load
   for (unsigned int y = 1; y <= n; ++y, start = 1 - start) {
     leftmost = neigh[idx(0, y, width)];
-    for (unsigned int x = start; x < width - (1 - start); x += 8) {
+    for (unsigned int x = start; x < width - (1 - start); x += 4) {
       int index = idx(x, y, width);
       // In haswell it is a tad better to load two 128 vectors when unaligned
       // See 14.6.2 at intel IA-32 Architectures Optimization Reference Manual
-      __m256 f = fload2x4(&same0[index]);
-      __m256 u = fload2x4(&neigh[index - width]);
-      __m256 r = fload2x4(&neigh[index - start + 1]);
-      __m256 d = fload2x4(&neigh[index + width]);
-      __m256 l = _mm256_blend_ps(fshl(r), fset1(leftmost), 0b00000001);
+      __m128 f = fload2x4(&same0[index]);
+      __m128 u = fload2x4(&neigh[index - width]);
+      __m128 r = fload2x4(&neigh[index - start + 1]);
+      __m128 d = fload2x4(&neigh[index + width]);
+      __m128 l = _mm_blend_ps(fshl(r), fset1(leftmost), 0b0001);
 
       // t = (f + a * (u + r + d + l)) / c
-      __m256 t = fmul(ffmadd(pa, fadd(u, fadd(r, fadd(d, l))), f), pinvc);
+      __m128 t = fmul(ffmadd(pa, fadd(u, fadd(r, fadd(d, l))), f), pinvc);
       fstore(&same[index], t);
 
       // extract the rightmost to be the next leftmost
-      _MM_EXTRACT_FLOAT(leftmost, _mm256_extractf128_ps(r, 1), 3);
+      _MM_EXTRACT_FLOAT(leftmost, r, 3);
     }
   }
 }
@@ -111,32 +111,32 @@ static void advect_rb(grid_color color, unsigned int n, float *samed,
   // https://stackoverflow.com/questions/53905757/what-is-the-difference-between-mm512-load-epi32-and-mm512-load-si512
   // TODO: See agner fog optimization manuals for generating constants
   // maybe using that we can free some vector registers
-  const __m256i psuccs = iset(7, 6, 5, 4, 3, 2, 1, 0);
-  const __m256 pdt0 = fset1(dt0);
-  const __m256 plowest = fset1(0.5f);
-  const __m256 phighest = fset1(n + 0.5f);
-  const __m256i pone = iset1(1);
-  const __m256 pfone = fset1(1.0);
-  const __m256i pnplus2 = iset1(n + 2);
-  const __m256i pwidth = iset1(width);
-  const __m256i phalfgrid = imul(pnplus2, pwidth);
+  const __m128i psuccs = iset(3, 2, 1, 0);
+  const __m128 pdt0 = fset1(dt0);
+  const __m128 plowest = fset1(0.5f);
+  const __m128 phighest = fset1(n + 0.5f);
+  const __m128i pone = iset1(1);
+  const __m128 pfone = fset1(1.0);
+  const __m128i pnplus2 = iset1(n + 2);
+  const __m128i pwidth = iset1(width);
+  const __m128i phalfgrid = imul(pnplus2, pwidth);
   for (int iy = 1; iy <= (int)n; iy++, shift = -shift, start = 1 - start) {
-    const __m256i pshift = iset1(shift);
-    const __m256i pstart = iset1(start);
-    const __m256i pi = iset1(iy);
-    for (int ix = start; ix < width - (1 - start); ix += 8) {
-      __m256i pj = iadd(iset1(ix), psuccs);  // j = x + 0, ..., x + 7
+    const __m128i pshift = iset1(shift);
+    const __m128i pstart = iset1(start);
+    const __m128i pi = iset1(iy);
+    for (int ix = start; ix < width - (1 - start); ix += 4) {
+      __m128i pj = iadd(iset1(ix), psuccs);  // j = x + 0, ..., x + 7
 
       int index = idx(ix, iy, width);
-      const __m256i pgridi = pi;
-      const __m256 pfgridi = itof(pgridi);  // (float)gridi
-      const __m256i pgridj =                // 2 * j + shift + start
-          iadd(_mm256_slli_epi32(pj, 1), iadd(pshift, pstart));
-      const __m256 pfgridj = itof(pgridj);  // (float)gridj
-      const __m256 psameu0 = fload(&sameu0[index]);
-      const __m256 psamev0 = fload(&samev0[index]);
-      __m256 px = ffnmadd(pdt0, psameu0, pfgridj);  // gridj - dt0 * sameu0[i]
-      __m256 py = ffnmadd(pdt0, psamev0, pfgridi);  // gridi - dt0 * samev0[i]
+      const __m128i pgridi = pi;
+      const __m128 pfgridi = itof(pgridi);  // (float)gridi
+      const __m128i pgridj =                // 2 * j + shift + start
+          iadd(_mm_slli_epi32(pj, 1), iadd(pshift, pstart));
+      const __m128 pfgridj = itof(pgridj);  // (float)gridj
+      const __m128 psameu0 = fload(&sameu0[index]);
+      const __m128 psamev0 = fload(&samev0[index]);
+      __m128 px = ffnmadd(pdt0, psameu0, pfgridj);  // gridj - dt0 * sameu0[i]
+      __m128 py = ffnmadd(pdt0, psamev0, pfgridi);  // gridi - dt0 * samev0[i]
 
       // TODO: Idea, as suggested in
       // https://stackoverflow.com/questions/38006616/how-to-use-if-condition-in-intrinsics
@@ -148,80 +148,80 @@ static void advect_rb(grid_color color, unsigned int n, float *samed,
       px = fclamp(px, plowest, phighest);
       py = fclamp(py, plowest, phighest);
 
-      const __m256i pj0 = ftoi(px);  // j0 = (int)x;
-      const __m256i pi0 = ftoi(py);  // i0 = (int)y;
+      const __m128i pj0 = ftoi(px);  // j0 = (int)x;
+      const __m128i pi0 = ftoi(py);  // i0 = (int)y;
 
-      const __m256 ps1 = _mm256_sub_ps(px, itof(pj0));  // s1 = x - j0;
-      const __m256 ps0 = _mm256_sub_ps(pfone, ps1);     // s0 = 1 - s1;
-      const __m256 pt1 = _mm256_sub_ps(py, itof(pi0));  // t1 = y - i0;
-      const __m256 pt0 = _mm256_sub_ps(pfone, pt1);     // t0 = 1 - t1;
+      const __m128 ps1 = fsub(px, itof(pj0));  // s1 = x - j0;
+      const __m128 ps0 = fsub(pfone, ps1);     // s0 = 1 - s1;
+      const __m128 pt1 = fsub(py, itof(pi0));  // t1 = y - i0;
+      const __m128 pt0 = fsub(pfone, pt1);     // t0 = 1 - t1;
 
       // Let's build IX(j0, i0):
-      const __m256i pisoddrow = iand(pi0, pone);         // i % 2
-      const __m256i pisevenrow = ixor(pisoddrow, pone);  // !isoddrow
+      const __m128i pisoddrow = iand(pi0, pone);         // i % 2
+      const __m128i pisevenrow = ixor(pisoddrow, pone);  // !isoddrow
 
       // (!= parity) === (j0 % 2) ^ (i0 % 2) === (j0 & 1) ^ (i0 & 1)
-      const __m256i pisblack = ixor(iand(pj0, pone), pisoddrow);
-      const __m256i pisred = ixor(pisblack, pone);     // !isblack
-      const __m256i pfshift = isub(pisred, pisblack);  // isred ? 1 : -1
+      const __m128i pisblack = ixor(iand(pj0, pone), pisoddrow);
+      const __m128i pisred = ixor(pisblack, pone);     // !isblack
+      const __m128i pfshift = isub(pisred, pisblack);  // isred ? 1 : -1
 
       // TODO: All the bool operations are using entire int registers,
       // can we improve on that?
       // !((isred && isevenrow) || (isblack && isoddrow)), or equivalently
       // (isblack || isoddrow) && (isred || isevenrow)
-      const __m256i p_starts_at_zero =
+      const __m128i p_starts_at_zero =
           iand(ior(pisblack, pisoddrow), ior(pisred, pisevenrow));
 
       // TODO: Maybe instead of a multiplication with
       // pisblack we could do a conditional move?
       // pbase = isblack ? (n+2) * width : 0
-      const __m256i pbase = imul(pisblack, phalfgrid);
-      const __m256i poffset = iadd(   // (j0 / 2) + i0 * ((n+2) / 2)
-          _mm256_srai_epi32(pj0, 1),  // (j0 / 2)
+      const __m128i pbase = imul(pisblack, phalfgrid);
+      const __m128i poffset = iadd(   // (j0 / 2) + i0 * ((n+2) / 2)
+          _mm_srai_epi32(pj0, 1),  // (j0 / 2)
           imul(pi0, pwidth)           // i0 * ((n+2) / 2)
       );
 
       // i0j0 = // IX(j0, i0)
-      const __m256i pi0j0 = iadd(pbase, poffset);
+      const __m128i pi0j0 = iadd(pbase, poffset);
       // i1j1 = i0j0 + width + (1 - isoffstart);
-      const __m256i pi1j1 = iadd(pi0j0, iadd(pwidth, p_starts_at_zero));
+      const __m128i pi1j1 = iadd(pi0j0, iadd(pwidth, p_starts_at_zero));
       // i0j1 = i0j0 + fshift * width * (n + 2) + (1 - isoffstart);
-      const __m256i pi0j1 =
+      const __m128i pi0j1 =
           iadd(pi0j0, iadd(p_starts_at_zero, imul(pfshift, phalfgrid)));
       // i1j0 = i0j0 + fshift * width * (n + 2) + width;
-      const __m256i pi1j0 = iadd(pi0j0, iadd(pwidth, imul(pfshift, phalfgrid)));
+      const __m128i pi1j0 = iadd(pi0j0, iadd(pwidth, imul(pfshift, phalfgrid)));
 
       // TODO: Gather ps seems to be slower on zx81 but faster on i7 7700hq
       // Read and test with:
       // https://stackoverflow.com/questions/24756534/in-what-situation-would-the-avx2-gather-instructions-be-faster-than-individually
       // So maybe we shouldn't use gather
-      const __m256 pd0i0j0 = _mm256_i32gather_ps(d0, pi0j0, 4);
-      const __m256 pd0i0j1 = _mm256_i32gather_ps(d0, pi0j1, 4);
-      const __m256 pd0i1j0 = _mm256_i32gather_ps(d0, pi1j0, 4);
-      const __m256 pd0i1j1 = _mm256_i32gather_ps(d0, pi1j1, 4);
+      const __m128 pd0i0j0 = fgather(d0, pi0j0, 4);
+      const __m128 pd0i0j1 = fgather(d0, pi0j1, 4);
+      const __m128 pd0i1j0 = fgather(d0, pi1j0, 4);
+      const __m128 pd0i1j1 = fgather(d0, pi1j1, 4);
       // s0 * (t0 * d0[i0j0] + t1 * d0[i1j0]) +
       // s1 * (t0 * d0[i0j1] + t1 * d0[i1j1])
-      const __m256 psamed =
+      const __m128 psamed =
           fadd(fmul(ps0, fadd(fmul(pt0, pd0i0j0), fmul(pt1, pd0i1j0))),
                fmul(ps1, fadd(fmul(pt0, pd0i0j1), fmul(pt1, pd0i1j1))));
 
-      const __m256 pu0i0j0 = _mm256_i32gather_ps(u0, pi0j0, 4);
-      const __m256 pu0i0j1 = _mm256_i32gather_ps(u0, pi0j1, 4);
-      const __m256 pu0i1j0 = _mm256_i32gather_ps(u0, pi1j0, 4);
-      const __m256 pu0i1j1 = _mm256_i32gather_ps(u0, pi1j1, 4);
+      const __m128 pu0i0j0 = fgather(u0, pi0j0, 4);
+      const __m128 pu0i0j1 = fgather(u0, pi0j1, 4);
+      const __m128 pu0i1j0 = fgather(u0, pi1j0, 4);
+      const __m128 pu0i1j1 = fgather(u0, pi1j1, 4);
       // s0 * (t0 * u0[i0j0] + t1 * u0[i1j0]) +
       // s1 * (t0 * u0[i0j1] + t1 * u0[i1j1])
-      const __m256 psameu =
+      const __m128 psameu =
           fadd(fmul(ps0, fadd(fmul(pt0, pu0i0j0), fmul(pt1, pu0i1j0))),
                fmul(ps1, fadd(fmul(pt0, pu0i0j1), fmul(pt1, pu0i1j1))));
 
-      const __m256 pv0i0j0 = _mm256_i32gather_ps(v0, pi0j0, 4);
-      const __m256 pv0i0j1 = _mm256_i32gather_ps(v0, pi0j1, 4);
-      const __m256 pv0i1j0 = _mm256_i32gather_ps(v0, pi1j0, 4);
-      const __m256 pv0i1j1 = _mm256_i32gather_ps(v0, pi1j1, 4);
+      const __m128 pv0i0j0 = fgather(v0, pi0j0, 4);
+      const __m128 pv0i0j1 = fgather(v0, pi0j1, 4);
+      const __m128 pv0i1j0 = fgather(v0, pi1j0, 4);
+      const __m128 pv0i1j1 = fgather(v0, pi1j1, 4);
       // s0 * (t0 * v0[i0j0] + t1 * v0[i1j0])
       // s1 * (t0 * v0[i0j1] + t1 * v0[i1j1])
-      const __m256 psamev =
+      const __m128 psamev =
           fadd(fmul(ps0, fadd(fmul(pt0, pv0i0j0), fmul(pt1, pv0i1j0))),
                fmul(ps1, fadd(fmul(pt0, pv0i0j1), fmul(pt1, pv0i1j1))));
 
@@ -262,20 +262,20 @@ static void project_rb_step1(unsigned int n, grid_color color,
   unsigned int start = color == RED ? 0 : 1;
   unsigned int width = (n + 2) / 2;
 
-  const __m256 zeros = fset1(0.0f);
+  const __m128 zeros = fset1(0.0f);
   for (unsigned int i = 1; i <= n; ++i, start = 1 - start)
-    for (unsigned int j = start; j < width - (1 - start); j += 8)
+    for (unsigned int j = start; j < width - (1 - start); j += 4)
       fstore(&sameu0[idx(j, i, width)], zeros);
 
-  const __m256 ratio = fset1(-0.5f / n);
+  const __m128 ratio = fset1(-0.5f / n);
   for (unsigned int i = 1; i <= n; ++i, start = 1 - start) {
-    for (unsigned int j = start; j < width - (1 - start); j += 8) {
+    for (unsigned int j = start; j < width - (1 - start); j += 4) {
       int index = idx(j, i, width);
-      __m256 u = fload2x4(&neighv[index - width]);
-      __m256 r = fload2x4(&neighu[index - start + 1]);
-      __m256 d = fload2x4(&neighv[index + width]);
-      __m256 l = fload2x4(&neighu[index - start]);
-      __m256 result = fmul(ratio, fadd(fsub(r, l), fsub(d, u)));
+      __m128 u = fload2x4(&neighv[index - width]);
+      __m128 r = fload2x4(&neighu[index - start + 1]);
+      __m128 d = fload2x4(&neighv[index + width]);
+      __m128 l = fload2x4(&neighu[index - start]);
+      __m128 result = fmul(ratio, fadd(fsub(r, l), fsub(d, u)));
       fstore(&samev0[index], result);
     }
   }
@@ -286,18 +286,18 @@ static void project_rb_step2(unsigned int n, grid_color color,
                              float *restrict neighu0) {
   unsigned int start = color == RED ? 0 : 1;
   unsigned int width = (n + 2) / 2;
-  const __m256 ratio = fset1(0.5f * n);
+  const __m128 ratio = fset1(0.5f * n);
   for (unsigned int i = 1; i <= n; ++i, start = 1 - start) {
-    for (unsigned int j = start; j < width - (1 - start); j += 8) {
+    for (unsigned int j = start; j < width - (1 - start); j += 4) {
       int index = idx(j, i, width);
-      __m256 oldu = fload2x4(&sameu[index]);
-      __m256 oldv = fload2x4(&samev[index]);
-      __m256 u = fload2x4(&neighu0[index - width]);
-      __m256 r = fload2x4(&neighu0[index - start + 1]);
-      __m256 d = fload2x4(&neighu0[index + width]);
-      __m256 l = fload2x4(&neighu0[index - start]);
-      __m256 newu = ffnmadd(ratio, fsub(r, l), oldu);
-      __m256 newv = ffnmadd(ratio, fsub(d, u), oldv);
+      __m128 oldu = fload2x4(&sameu[index]);
+      __m128 oldv = fload2x4(&samev[index]);
+      __m128 u = fload2x4(&neighu0[index - width]);
+      __m128 r = fload2x4(&neighu0[index - start + 1]);
+      __m128 d = fload2x4(&neighu0[index + width]);
+      __m128 l = fload2x4(&neighu0[index - start]);
+      __m128 newu = ffnmadd(ratio, fsub(r, l), oldu);
+      __m128 newv = ffnmadd(ratio, fsub(d, u), oldv);
       fstore(&sameu[index], newu);
       fstore(&samev[index], newv);
     }
