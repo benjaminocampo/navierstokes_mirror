b1869dd6cda3a0e90480735804f3074557175ca1
Mateo de Mayo <mateodemayo@gmail.com>
2020-05-13 19:37:43 -0300

    Make cache graph function adapt to varying comparisson sizes

--- charts.py
+++ charts.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from os import makedirs
 from os.path import exists
-from itertools import count, cycle
+from itertools import count, cycle, islice
 
 white = "#FFFFFF"
 darkgray = "#263238"
@@ -53,11 +53,13 @@ def save_nscell_graph(names, means, ns, iterations, filename="plot.png", only_sh
         """Attach a text label above each bar in *rects*, displaying its height."""
         for rect in rects:
             height = rect.get_height()
-            ax.annotate("{}".format(height),
-                        xy=(rect.get_x() + rect.get_width() / 2, height),
-                        xytext=(0, -16),
-                        textcoords="offset points",
-                        ha="center", va="center", color=white, fontsize=bar_fontsize)
+            ax.annotate(
+                "{}".format(height),
+                xy=(rect.get_x() + rect.get_width() / 2, height),
+                xytext=(0, -16),
+                textcoords="offset points",
+                ha="center", va="center", color=white, fontsize=bar_fontsize
+            )
 
     x = np.arange(nof_groups)  # the label locations
     fig, ax = plt.subplots(figsize=(img_width / DPI, img_height / DPI), dpi=DPI)
@@ -85,11 +87,29 @@ def save_nscell_graph(names, means, ns, iterations, filename="plot.png", only_sh
     plt.show() if only_show else plt.savefig(filename)
     plt.close()
 
-def save_cache_graph(source_name, target_name, source_refs, source_misses, target_refs, target_misses, ns, iterations, filename="plot.png", cache_name="L1", only_show=False):
-    source_color = "#FFC107" if cache_name == "L1" else "#00BCD4"
-    source_color_dark = "#FFA000" if cache_name == "L1" else "#0097A7"
-    target_color = "#E91E63" if cache_name == "L1" else "#673AB7"
-    target_color_dark = "#C2185B" if cache_name == "L1" else "#512DA8"
+def save_cache_graph(names, refss, missess, ns, iterations, filename="plot.png", cache_name="L1", only_show=False):
+    CATEGORY_WIDTH_PX = 432 # Magic number that correlates to bar_width
+    DPI = 150
+    bar_fontsize = 10
+    legend_fontsize = 6
+    nof_runs = len(names)
+    nof_groups = len(ns)
+    img_width = 96 * nof_groups * nof_runs
+    img_height = 720
+    bar_width = CATEGORY_WIDTH_PX / img_width
+
+    def autolabel(rects, total=False):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate(
+                f"{human_readable(height if not total else height + rect.get_y())}",
+                xy=(rect.get_x() + rect.get_width() / 2, rect.get_y() + height),
+                xytext=(0, -16 if not total else 8),
+                textcoords="offset points",
+                ha="center", va="center", color=white if not total else darkgray, fontsize=bar_fontsize
+            )
+
     def human_readable(number):
         multiples = [1e3, 1e6, 1e9, 1e12]
         units = ["K", "M", "G", "T"]
@@ -100,55 +120,46 @@ def save_cache_graph(source_name, target_name, source_refs, source_misses, targe
             if number >= m:
                 number /= m
                 unit = u
-        return f"{number:.0f}{unit}" if number >= 100 else f"{number:.2f}{unit}"
+        if number >= 100:
+            return f"{number:.0f}{unit}"
+        elif number >= 10:
+            return f"{number:.1f}{unit}"
+        else:
+            return f"{number:.2f}{unit}"
 
-    x = np.arange(len(ns))  # the label locations
-    width = 0.45  # the width of the bars
+    x = np.arange(nof_groups)  # the label locations
+    hitss = [refs - misses for refs, misses in zip(refss, missess)]
+    scaled_hitss = [hits / ((ns**2) * iterations) for hits in hitss]
+    scaled_missess = [misses / ((ns**2) * iterations) for misses in missess]
 
-    source_hits = source_refs - source_misses
-    target_hits = target_refs - target_misses
-
-    scaled_source_hits = source_hits / ((ns**2) * iterations)
-    scaled_source_misses = source_misses / ((ns**2) * iterations)
-    scaled_target_hits = target_hits / ((ns**2) * iterations)
-    scaled_target_misses = target_misses / ((ns**2) * iterations)
-
-
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(img_width / DPI, img_height / DPI), dpi=DPI)
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.spines["bottom"].set_color(darkgray)
     ax.spines["left"].set_color(darkgray)
-    source_misses_bar = ax.bar(x - width/2, scaled_source_misses, width, bottom=scaled_source_hits, label=f"{source_name} misses", color=source_color_dark)
-    source_hits_bar = ax.bar(x - width/2, scaled_source_hits, width, label=f"{source_name} hits", color=source_color)
-    target_misses_bar = ax.bar(x + width/2, scaled_target_misses, width, bottom=scaled_target_hits, label=f"{target_name} misses", color=target_color_dark)
-    target_hits_bar = ax.bar(x + width/2, scaled_target_hits, width, label=f"{target_name} hits", color=target_color)
+
+    starting_x = x - nof_runs / 2 + 0.5
+    misses_bars = []
+    hits_bars = []
+    cyclec = lambda c: islice(cycle(c), 2 if cache_name == "L1" else 4, None)
+    for i, name, misses, hits, color, dark_color in zip(count(), names, scaled_missess, scaled_hitss, cyclec(COLORS), cyclec(DARK_COLORS)):
+        hit_bar = ax.bar(starting_x + bar_width * i, hits, bar_width, label=f"{name} hits", color=color)
+        hits_bars.append(hit_bar)
+        autolabel(hit_bar)
+        miss_bar = ax.bar(starting_x + bar_width * i, misses, bar_width, bottom=hits, label=f"{name} misses", color=dark_color)
+        misses_bars.append(miss_bar)
+        autolabel(miss_bar, True)
+
     ax.set_ylabel(f"{cache_name} References")
     ax.set_title(f"Cache {cache_name} References per Cell Iteration")
-    plt.figtext(0.965, 0.5, 'Lower is better', ha='center', va="center", fontsize=6, rotation=-90)
-    ax.set_xticks(x)
+    plt.figtext(0.965, 0.5, 'Lower is better', ha='center', va="center", fontsize=legend_fontsize, rotation=-90)
+    tick_xs = [(i - nof_runs / 2 + 0.5) + (nof_runs - 1) * (bar_width / 2) for i in x]
+    ax.set_xticks(tick_xs)
     ax.set_xticklabels([f"N={s} x{i}" for s, i in zip(ns, iterations)])
-    ax.legend(loc="lower left", fontsize=6)
-
-
-    def autolabel(rects, total=False):
-        """Attach a text label above each bar in *rects*, displaying its height."""
-        FONTSIZE = 10
-        for rect in rects:
-            height = rect.get_height()
-            ax.annotate(f"{human_readable(height if not total else height + rect.get_y())}",
-                        xy=(rect.get_x() + rect.get_width() / 2, rect.get_y() + height),
-                        xytext=(0, -16 if not total else 8),
-                        textcoords="offset points",
-                        ha="center", va="center", color=white if not total else darkgray, fontsize=FONTSIZE)
-
-    autolabel(source_hits_bar)
-    autolabel(source_misses_bar, True)
-    autolabel(target_hits_bar)
-    autolabel(target_misses_bar, True)
+    ax.legend(loc="lower left", fontsize=legend_fontsize)
 
     fig.tight_layout()
-    plt.show() if only_show else plt.savefig(filename, dpi=150)
+    plt.show() if only_show else plt.savefig(filename)
     plt.close()
 
 def read_perfstats(filename, stats, cast_to=int):
@@ -229,7 +240,9 @@ def main():
             llc_refs.append(np.array([e["cache-references"] for e in run_measuremets[run]]))
             llc_misses.append(np.array([e["cache-misses"] for e in run_measuremets[run]]))
 
-        save_nscell_graph(names, means, ns, steps, f"{plotpath}/nspcellgraph__{plotid}.png", only_show=False)
+        save_nscell_graph(names, means, ns, steps, f"{plotpath}/nspcellgraph__{plotid}.png", only_show=True)
+        save_cache_graph(names, l1_refs, l1_misses, ns, steps, filename=f"{plotpath}/l1graph__{plotid}.png", cache_name="L1", only_show=True)
+        save_cache_graph(names, llc_refs, llc_misses, ns, steps, filename=f"{plotpath}/llcgraph__{plotid}.png", cache_name="LLC", only_show=True)
 
 if __name__ == "__main__":
     main()
