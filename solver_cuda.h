b23bcfa5ca5e8c9f4dded4a79730e4f258f419a5
Nicolás Benjamín Ocampo <cp202004@zx81.famaf.unc.edu.ar>
2020-06-15 14:42:20 -0300

    Remove unused host functions that were duplicated in solver_cuda.{c, h}

--- solver_cuda.h
+++ solver_cuda.h
@@ -3,9 +3,6 @@
 __global__
 void gpu_add_source(unsigned int n, float *x, const float *s, float dt);
 
-void add_source(unsigned int n, float *x, const float *s, float dt,
-                const unsigned int from, const unsigned int to);
-
 __global__
 void gpu_set_bnd(unsigned int n, boundary b, float *x);
 
@@ -14,34 +11,16 @@ void gpu_lin_solve_rb_step(grid_color color,unsigned int n, float a, float c,
                            const float * same0, const float * neigh,
                            float * same);
 
-void lin_solve_rb_step(grid_color color, unsigned int n, float a, float c,
-                       const float *same0, const float *neigh, float *same,
-                       const unsigned int from, const unsigned int to);
-
-void advect_rb(grid_color color, unsigned int n, float *samed, float *sameu,
-               float *samev, const float *samed0, const float *sameu0,
-               const float *samev0, const float *d0, const float *u0,
-               const float *v0, float dt, const unsigned int from,
-               const unsigned int to);
-
 __global__
 void gpu_advect_rb(grid_color color, unsigned int n, float dt,
                    float *samed, float *sameu, float *samev,
                    const float *samed0, const float *sameu0, const float *samev0,
                    const float *d0, const float *u0, const float *v0);
 
-void project_rb_step1(unsigned int n, grid_color color, float *sameu0,
-                      float *samev0, float *neighu, float *neighv,
-                      const unsigned int from, const unsigned int to);
-
 __global__
 void gpu_project_rb_step1(unsigned int n, grid_color color,
                           float *sameu0, float *samev0,
                           float *neighu, float *neighv);
-
-void project_rb_step2(unsigned int n, grid_color color, float *sameu,
-                      float *samev, float *neighu0, const unsigned int from,
-                      const unsigned int to);
 
 __global__
 void gpu_project_rb_step2(unsigned int n, grid_color color,
